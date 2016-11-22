97ce20e16e2021a7b75f5970bc30903ca7e95abe
aleques-itj <alex@fa.ke>
2016-11-22 10:12:35 -0500

    Cleanup

--- Framework/CRT/Win32/free.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include "Core/PF_MinWin.h"
-
-
-void free(void* ptr)
-{
-	HeapFree(GetProcessHeap(), 0, ptr);
-}
--- Framework/CRT/Win32/malloc.cpp
+++
@@ -1,8 +0,0 @@
-#include <stdlib.h>
-#include "Core/PF_MinWin.h"
-
-
-void* malloc(size_t size)
-{
-	return HeapAlloc(GetProcessHeap(), 0, size);
-}
